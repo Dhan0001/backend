22c3ed576e47819b1e69f4f42dcf9a14e619d5e2
Dhan0001 <richdhanroca294@gmai.com>
2023-10-23 13:24:56 +0800

    Added UserController API

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 
 use App\Http\Controllers\Api\CarouselController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +28,8 @@ Route::get('/carousels/{id}', [CarouselController::class, 'show']);
 Route::post('/carousels', [CarouselController::class, 'store']);
 Route::put('/carousels/{id}', [CarouselController::class, 'update']);
 Route::delete('/carousels/{id}', [CarouselController::class, 'destroy']);
+
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::get('/user', [UserController::class, 'index']);
+Route::post('/user', [UserController::class, 'store']);
